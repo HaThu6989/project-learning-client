84725705bc6fc198d873b63febed7a4aa063e082
Ha Thu <tranhathu6989@gmail.com>
2022-05-25 15:50:23 +0200

    Homepage carousel

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,12 +3,18 @@ import NavbarMenu from "./NavbarMenu";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
+import carousel1 from "../../src/assets/carousel1.webp";
+import carousel2 from "../../src/assets/carousel2.jpg";
+import carousel3 from "../../src/assets/carousel3.jpg";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
 function Landing() {
   return (
     <>
       <NavbarMenu />
 
-      <Container className="mt-5 p-3 background-overlay-landing">
+      <Container className="my-4 mb-3 p-3 background-overlay-landing">
         <Row className="justify-content-center text-center ">
           <Col md="auto">
             <h1 className="mb-3">Website For E-Learning</h1>
@@ -17,19 +23,41 @@ function Landing() {
               publishing platform to keep track of what you are learning.
             </p>
             <p>
-              The Learner Dashboard is a centralized location where you can
-              quickly see where you are in a course, what action items you may
-              still have, and what courses you are currently enrolled in. Upon
-              login, you will automatically be directed to your Learner
-              Dashboard.
+              The Learner Dashboard is a website where you can quickly see where
+              you are in a course, what action items you may still have, and
+              what courses you are currently enrolled in. Upon login, you will
+              automatically be directed to your Learner Dashboard.
             </p>
             <p>
-              Please note: Learners will only see the tabs and sections as
-              applicable to them and their institution.{" "}
+              <strong> Please note</strong>: Learners will only see the tabs and
+              sections as applicable to them and their institution.{" "}
             </p>
           </Col>
         </Row>
       </Container>
+      <Carousel className="d-flex justify-content-center">
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto w-20"
+            src={carousel1}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto w-20"
+            src={carousel2}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block mx-auto w-20"
+            src={carousel3}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+      </Carousel>
     </>
   );
 }
